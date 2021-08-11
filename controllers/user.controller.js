3416872574c42b52f9d681ddc80e19f645cf61cc
Davis <davisrdln@gmail.com>
2021-08-11 01:09:28 -0600

    se agregaron detalles

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,19 +6,23 @@ var md5 = require("md5")
 var jwt = require("../services/jwt");
 
 function login(req,res){
-    let userName = req.body.userName;
-    let password = md5(req.body.password);
-    let query1 = 'call Sp_ValidarLogin("'+userName+'","'+password+'")';
-    
-    conexion.query(query1, (err, findUser)=>{
-        if(err){
-            res.send({message:"error general"});
-        }else if(findUser){
-            res.send({message:"Usuario logueado", findUser,token: jwt.createToken(findUser)});
-        }else{
-            res.send({message:"no se ha econtrado un usuario con este usuario o password"})
-        }
-    });
+    if(req.body.userName && req.body.usuarioContrasena){
+        let userName = req.body.userName;
+        let password = md5(req.body.usuarioContrasena);
+        let query1 = 'call Sp_ValidarLogin("'+userName+'","'+password+'")';
+        
+        conexion.query(query1, (err, findUser)=>{
+            if(err){
+                res.send({message:"error general"});
+            }else if(findUser){
+                res.send({message:"Usuario logueado", findUser,token: jwt.createToken(findUser)});
+            }else{
+                res.send({message:"no se ha econtrado un usuario con este usuario o password"})
+            }
+        });
+    }else{
+        res.send({message:"Ingresa los campos obligatorios"})
+    }
 }
 
 module.exports ={
